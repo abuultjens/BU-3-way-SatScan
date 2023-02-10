a9483f83b8c7ad0951da91452b2930c4b52adc39
Andrew Buultjens <buultjensa@gmail.com>
2023-02-10 15:33:55 +1100

    Add files via upload

--- shp_comp.sh
+++ shp_comp.sh
@@ -9,7 +9,7 @@ for TAXA in $(cat $1); do
 	Rscript determine_overlap.R \
 	RAND_${TAXA} \
 	summer_MP_SCAT/RAND_${TAXA}.col.shp \
-	177_data_for_SatScan_TRAPS/RAND_${TAXA}.col.shp \
+	177_TRAPS_AGAIN/RAND_${TAXA}.col.shp \
 	2023_MP_CASES_01.01.2019-28.12.2020/RAND_${TAXA}.col.shp
 	
 	n_A_B_C=`cat n_A_B_C.txt`
